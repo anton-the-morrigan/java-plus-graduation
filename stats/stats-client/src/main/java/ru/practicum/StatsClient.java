15960731b38993bdca71e1d84dec99f52927c7e4
anton-the-morrigan <anton.the.morrigan@yandex.ru>
2025-11-20 03:49:10 +0300

    fix

--- stats/stats-client/src/main/java/ru/practicum/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/StatsClient.java
@@ -1,7 +1,8 @@
 package ru.practicum;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
@@ -14,25 +15,25 @@ import java.util.Arrays;
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class StatsClient {
-
     private final RestTemplate restTemplate;
+    private final DiscoveryClient discoveryClient;
 
-    private final String serverUrl;
-
-    @Autowired
-    public StatsClient(RestTemplate restTemplate,@Value("${stats.server.url}") String serverUrl) {
-        this.restTemplate = restTemplate;
-        this.serverUrl = serverUrl;
-    }
+    private final String STATS_SERVER_ID = "stats-server";
 
     public void postHit(EndpointHitDto dto) {
-        restTemplate.postForEntity(serverUrl + "/hit", dto, Void.class);
+        ServiceInstance statsServer = getInstance();
+        restTemplate.postForEntity(statsServer.getUri() + "/hit", dto, Void.class);
     }
 
     public List<ViewStatsDto> getStats(String start, String end, List<String> uris, boolean unique) throws RestClientException {
 
-        StringBuilder uri = new StringBuilder(serverUrl).append("/stats")
+        ServiceInstance serviceInstance = getInstance();
+
+        StringBuilder uri = new StringBuilder()
+                .append(serviceInstance.getUri())
+                .append("/stats")
                 .append("?start=").append(start)
                 .append("&end=").append(end)
                 .append("&unique=").append(unique);
@@ -47,5 +48,18 @@ public class StatsClient {
 
         ViewStatsDto[] body = response.getBody();
         return (body == null) ? new ArrayList<>() : Arrays.asList(body);
+    }
+
+    private ServiceInstance getInstance() {
+        try {
+            return discoveryClient
+                    .getInstances(STATS_SERVER_ID)
+                    .getFirst();
+        } catch (Exception exception) {
+            throw new RuntimeException(
+                    "Ошибка обнаружения адреса сервиса статистики с id: " + STATS_SERVER_ID,
+                    exception
+            );
+        }
     }
 }
