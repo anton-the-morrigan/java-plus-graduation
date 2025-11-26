a7177c9bb531a7233d58fa6795ce3040cb905234
anton-the-morrigan <anton.the.morrigan@yandex.ru>
2025-11-27 01:08:31 +0300

    fix

--- core/interaction-api/src/main/java/ru/practicum/client/RequestClient.java
+++ core/interaction-api/src/main/java/ru/practicum/client/RequestClient.java
@@ -1,6 +1,7 @@
 package ru.practicum.client;
 
 import feign.FeignException;
+import jakarta.validation.constraints.Positive;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.dto.request.EventRequestStatusUpdateRequest;
@@ -13,11 +14,11 @@ import java.util.Map;
 @FeignClient(name = "request-service")
 public interface RequestClient {
 
-    @GetMapping("/event/{eventId}")
-    List<ParticipationRequestDto> getRequestForEventByUserId(@PathVariable Long eventId, @RequestParam Long userId) throws FeignException;
+    @GetMapping("{userId}/events/event/{eventId}")
+    List<ParticipationRequestDto> getRequestForEventByUserId(@PathVariable @Positive Long eventId, @PathVariable @Positive Long userId) throws FeignException;
 
-    @PatchMapping
-    EventRequestStatusUpdateResult updateRequests(@RequestBody Long eventId, @RequestBody Long userId, @RequestBody EventRequestStatusUpdateRequest updateRequest) throws FeignException;
+    @PatchMapping("{userId}/events/{eventId}/requests")
+    EventRequestStatusUpdateResult updateRequests(@PathVariable @Positive Long eventId, @PathVariable @Positive Long userId, @RequestBody EventRequestStatusUpdateRequest updateRequest) throws FeignException;
 
     @GetMapping("/confirmed")
     Map<Long, Long> getConfirmedRequestsCount(@RequestParam List<Long> eventIds) throws FeignException;
