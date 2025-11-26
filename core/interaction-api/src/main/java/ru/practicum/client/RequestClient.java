6299b7a0af1e36ec4d194e44a8261e3d266ae831
anton-the-morrigan <anton.the.morrigan@yandex.ru>
2025-11-27 00:48:00 +0300

    fix

--- core/interaction-api/src/main/java/ru/practicum/client/RequestClient.java
+++ core/interaction-api/src/main/java/ru/practicum/client/RequestClient.java
@@ -2,8 +2,7 @@ package ru.practicum.client;
 
 import feign.FeignException;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.practicum.dto.request.EventRequestStatusUpdateRequest;
 import ru.practicum.dto.request.EventRequestStatusUpdateResult;
 import ru.practicum.dto.request.ParticipationRequestDto;
@@ -15,11 +14,11 @@ import java.util.Map;
 public interface RequestClient {
 
     @GetMapping("/event/{eventId}")
-    List<ParticipationRequestDto> getRequestForEventByUserId(Long eventId, Long userId) throws FeignException;
+    List<ParticipationRequestDto> getRequestForEventByUserId(@PathVariable Long eventId, Long userId) throws FeignException;
 
     @PatchMapping
-    EventRequestStatusUpdateResult updateRequests(Long eventId, Long userId, EventRequestStatusUpdateRequest updateRequest) throws FeignException;
+    EventRequestStatusUpdateResult updateRequests(@RequestBody Long eventId, Long userId, EventRequestStatusUpdateRequest updateRequest) throws FeignException;
 
     @GetMapping("/confirmed")
-    Map<Long, Long> getConfirmedRequestsCount(List<Long> eventIds) throws FeignException;
+    Map<Long, Long> getConfirmedRequestsCount(@RequestParam List<Long> eventIds) throws FeignException;
 }
