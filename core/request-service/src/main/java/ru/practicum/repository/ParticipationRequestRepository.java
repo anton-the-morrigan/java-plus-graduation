c786ea715be25aabca9552611ec904cc480b1d35
anton-the-morrigan <anton.the.morrigan@yandex.ru>
2025-11-27 01:26:30 +0300

    fix

--- core/request-service/src/main/java/ru/practicum/repository/ParticipationRequestRepository.java
+++ core/request-service/src/main/java/ru/practicum/repository/ParticipationRequestRepository.java
@@ -15,10 +15,10 @@ import java.util.stream.Collectors;
 public interface ParticipationRequestRepository extends JpaRepository<ParticipationRequest, Long> {
 
     @Query("""
-            select r.event.id, count(r.id)
+            select r.eventId, count(r.id)
             from ParticipationRequest r
-            where r.event.id in ?1 and r.status = ?2
-            group by r.event.id""")
+            where r.eventId in ?1 and r.status = ?2
+            group by r.eventId""")
     List<Object[]> countRequestsByStatus(List<Long> ids, RequestStatus status);
 
     long countByEventIdAndStatus(Long eventId, RequestStatus status);
