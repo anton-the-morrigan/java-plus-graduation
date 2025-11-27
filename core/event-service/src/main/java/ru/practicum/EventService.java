a32576ec5288639667aabee690e54845aba850b4
anton-the-morrigan <anton.the.morrigan@yandex.ru>
2025-11-27 04:23:37 +0300

    fix

--- core/event-service/src/main/java/ru/practicum/EventService.java
+++ core/event-service/src/main/java/ru/practicum/EventService.java
@@ -3,9 +3,13 @@ package ru.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
+import ru.practicum.config.ExceptionHandlingConfig;
+import ru.practicum.config.JacksonConfig;
 
 @EnableFeignClients
 @SpringBootApplication
+@Import({JacksonConfig.class, ExceptionHandlingConfig.class})
 public class EventService {
     public static void main(String[] args) {
         SpringApplication.run(EventService.class, args);
